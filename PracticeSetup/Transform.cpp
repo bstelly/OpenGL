ba049120adf7d944f5c315dd46bf98c9c32ab2c9
bstelly <bstelly1999@gmail.com>
2018-08-23 17:56:01 -0500

    implementing transform class

--- PracticeSetup/Transform.cpp
+++ PracticeSetup/Transform.cpp
@@ -2,12 +2,19 @@
 
 Transform::Transform()
 {
-	m_worldPosition = glm::vec3(0, 0, 0);
-	m_localPosition = glm::vec3(0, 0, 0);
-	m_worldScale = glm::vec3(0, 0, 0);
-	m_localScale = glm::vec3(0, 0, 0);
-	m_worldRotation = glm::vec3(0, 0, 0);
-	m_localRotation = glm::vec3(0, 0, 0);
+	m_model = glm::mat4(1);
+	
+	m_worldPosition = m_model[3];
+	m_localPosition = m_model[3];
+	
+	m_worldScale = glm::vec3(1, 1, 1);
+	m_localScale = glm::vec3(1, 1, 1);
+	m_worldRotation = glm::vec3(1);
+	for (int col = 0; col < 3; col++)
+	{
+		m_worldRotation = m_model[col];
+		m_localRotation = m_model[col];
+	}
 }
 
 Transform::~Transform()
@@ -17,15 +24,69 @@ Transform::~Transform()
 
 glm::mat4 Transform::Translate(glm::vec3 move)
 {
-
+	glm::mat4 translation = glm::mat4(1);
+	translation[3].xyz = move;
+	m_model = m_model * translation;
+	return m_model;
 }
 
 glm::mat4 Transform::Rotation(float radius, glm::vec3 axis)
 {
+	float cosine = cos(radius);
+	float sine = sin(radius);
 
+	glm::vec3 x_axis = glm::vec3{ 1, 0, 0 };
+	glm::vec3 y_axis = glm::vec3{ 0, 1, 0 };
+	glm::vec3 z_axis = glm::vec3{ 0, 0, 1 };
+
+
+
+
+
+
+
+
+
+
+
+
+	if (axis[0] == 0 && axis[1] == 0 && axis[2] == 1)
+	{
+		m_model = m_model * glm::mat4{
+			cos(radius), -sin(radius), 0, 0,	//rotate on Z
+			sin(radius), cos(radius), 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 0};
+		return m_model;
+	}
+
+	if (axis[0] == 1 && axis[1] == 0 && axis[2] == 0)
+	{
+		m_model = m_model * glm::mat4{
+			1, 0, 0, 0,
+			0, cos(radius), -sin(radius), 0,	//rotate on x
+			0, sin(radius), cos(radius), 0,
+			0, 0, 0, 0
+		};
+		return m_model;
+	}
+
+	if (axis[0] == 0 && axis[1] == 1 && axis[2] == 0)
+	{
+		m_model = m_model * glm::mat4{
+			cos(radius), 0, sin(radius), 0,		//rotate on y
+			0, 1, 0, 0,
+			-sin(radius), 0, cos(radius), 0,
+			0, 0, 0, 0
+		};
+		return m_model;
+	}
 }
 
 glm::mat4 Transform::Scale(float scale)
 {
-
+	m_model[0].x = scale;
+	m_model[1].y = scale;
+	m_model[2].z = scale;
+	return m_model;
 }
--- PracticeSetup/Transform.h
+++ PracticeSetup/Transform.h
@@ -1,6 +1,7 @@
 #pragma once
-#include <glm/fwd.hpp>
+#define GLM_FORCE_SWIZZLE
 #include <glm/ext.hpp>
+
 
 class Transform
 {
@@ -13,11 +14,15 @@ public:
 	glm::mat4 Scale(float scale);
 
 private:
-	glm::vec3 m_worldPosition;
-	glm::vec3 m_localPosition;
-	glm::vec3 m_worldScale;
-	glm::vec3 m_localScale;
-	glm::vec3 m_worldRotation;
-	glm::vec3 m_localRotation;
 
+	glm::mat4 m_model; //Identity
+	
+	glm::vec3 m_worldPosition; //m_model[3]
+	glm::vec3 m_localPosition;
+	
+	glm::vec3 m_worldScale; //Matrix diagonal
+	glm::vec3 m_localScale;
+	
+	glm::vec3 m_worldRotation; //3x3 matrix; X, Y, Z
+	glm::vec3 m_localRotation;
 };
