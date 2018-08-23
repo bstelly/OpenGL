83c31ad53bf770edf8bc17c45efe1e0fcbecc84e
bstelly <bstelly1999@gmail.com>
2018-08-23 18:07:03 -0500

    Implementing transform class

--- PracticeSetup/Transform.cpp
+++ PracticeSetup/Transform.cpp
@@ -39,7 +39,7 @@ glm::mat4 Transform::Rotation(float radius, glm::vec3 axis)
 	glm::vec3 y_axis = glm::vec3{ 0, 1, 0 };
 	glm::vec3 z_axis = glm::vec3{ 0, 0, 1 };
 
-
+	glm::mat4 rotationMatrix = glm::mat4{}
 
 
 
